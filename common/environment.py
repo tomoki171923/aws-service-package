e62e317b00b882bdaf5793b1dac0dd5ee5724007
tomoki <tomoki171923@gmail.com>
2021-07-24 13:36:51 +0900

    refacter by blank & flake8

--- common/environment.py
+++ common/environment.py
@@ -4,61 +4,61 @@ import os
 import os.path
 
 
-''' Checking whether this environment is the development or not
+""" Checking whether this environment is the development or not
 Returns:
     bool: True / False
-'''
+"""
 
 
 def isDev():
-    return os.environ.get("AWS_LAMBDA_FUNCTION_VERSION", "no_env") == '$LATEST'
+    return os.environ.get("AWS_LAMBDA_FUNCTION_VERSION", "no_env") == "$LATEST"
 
 
-''' Checking whether this environment is the staging or not
+""" Checking whether this environment is the staging or not
 Returns:
     bool: True / False
-'''
+"""
 
 
 def isSt():
-    return os.environ.get("AWS_LAMBDA_FUNCTION_ALIAS", "no_env") == 'st'
+    return os.environ.get("AWS_LAMBDA_FUNCTION_ALIAS", "no_env") == "st"
 
 
-''' Checking whether this environment is the production or not
+""" Checking whether this environment is the production or not
 Returns:
     bool: True / False
-'''
+"""
 
 
 def isPro():
-    return os.environ.get("AWS_LAMBDA_FUNCTION_ALIAS", "no_env") == 'pro'
+    return os.environ.get("AWS_LAMBDA_FUNCTION_ALIAS", "no_env") == "pro"
 
 
-''' Checking whether this environment is staging or development
+""" Checking whether this environment is staging or development
 Returns:
     bool: True / False
-'''
+"""
 
 
 def isDevOrSt():
     return isDev() or isSt()
 
 
-''' Checking whether this environment is the local
+""" Checking whether this environment is the local
 Returns:
     bool: True / False
-'''
+"""
 
 
 def isLocal():
-    return os.environ.get("AWS_LAMBDA_ENV", "no_env") == 'LOCAL'
+    return os.environ.get("AWS_LAMBDA_ENV", "no_env") == "LOCAL"
 
 
-''' Checking whether this environment is a docker container
+""" Checking whether this environment is a docker container
 Returns:
     bool: True / False
-'''
+"""
 
 
 def isDocker():
-    return os.path.exists('/.dockerenv')
+    return os.path.exists("/.dockerenv")
