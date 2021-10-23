35c13852d706d3e6e4cf045db461d403e81c2176
tomoki <tomoki171923@gmail.com>
2021-10-24 05:15:08 +0900

    add unittest code for sns & refactor

--- src/awspack/sns/sns.py
+++ src/awspack/sns/sns.py
@@ -4,7 +4,7 @@ This module has some functions to send an email using AWS SNS.
 """
 
 import boto3
-from ..lambdalib.environment import isPro, isDocker
+from ..lambdalib.environment import isDev, isSt, isLocal
 
 
 """ send an email in TEXT format.
@@ -13,6 +13,7 @@ Args:
     topic_arn (str): SNS topic arn
     subject (str): the subject on the email.
     message (str): the body in TEXT format on the email.
+    message_structure (str, optional): Set MessageStructure to json if you want to send a different message for each protocol.
     message_attributes (dict, optional): The user-specified message attribute value.
         For string data types, the value attribute has the same restrictions on the content as the message body.
 Returns:
@@ -25,31 +26,24 @@ def sendMessage(
     topic_arn: str,
     subject: str,
     message: str,
+    message_structure: str = None,
     message_attributes: dict = None,
 ) -> dict:
-    if isDocker():
-        client = boto3.client(
-            "sns",
-            region_name=region_name,
-            endpoint_url="http://host.docker.internal:4579",
-        )
-    else:
-        client = boto3.client("sns", region_name=region_name)
-    if not isPro():
-        subject = "【dev/st環境】" + subject
-    if message_attributes is None:
-        response = client.publish(
-            TopicArn=topic_arn,
-            Message=message,
-            Subject=subject,
-            MessageStructure="string",
-        )
-    else:
-        response = client.publish(
-            TopicArn=topic_arn,
-            Message=message,
-            Subject=subject,
-            MessageStructure="string",
-            MessageAttributes=message_attributes,
-        )
+    client = boto3.client("sns", region_name=region_name)
+    if isDev():
+        subject = "[dev] " + subject
+    if isSt():
+        subject = "[staging] " + subject
+    if isLocal():
+        subject = "[local] " + subject
+    kwargs: dict = {
+        "TopicArn": topic_arn,
+        "Message": message,
+        "Subject": subject,
+    }
+    if message_structure is not None:
+        kwargs["MessageStructure"] = message_structure
+    if message_attributes is not None:
+        kwargs["MessageAttributes"] = message_attributes
+    response = client.publish(**kwargs)
     return response
